e10e51b044b6b0693fc78817447eee0fadca1f2e
SkynetHome <hydrillagorilla@gmail.com>
2016-04-09 17:50:03 -0500

    Create Add Drop XML

--- app/src/main/java/com/adeptimpulse/bucketdrops/app/ActivityMain.java
+++ app/src/main/java/com/adeptimpulse/bucketdrops/app/ActivityMain.java
@@ -3,18 +3,29 @@ package com.adeptimpulse.bucketdrops.app;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 import com.bumptech.glide.Glide;
 
 public class ActivityMain extends AppCompatActivity {
 
     private Toolbar mToolbar;
+    Button mBtnAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
+        mBtnAdd = (Button) findViewById(R.id.btn_add);
+        mBtnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(ActivityMain.this, "BUTTON WAS CLICKED", Toast.LENGTH_LONG).show();
+            }
+        });
         setSupportActionBar(mToolbar);
         initBackgroundImage();
 
